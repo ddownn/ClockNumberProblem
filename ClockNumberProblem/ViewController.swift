c691956838eb78e8d008d6dacec5297503f5313d
Paul <ddownn@gmail.com>
2020-11-01 23:20:21 -0500

    Initial Commit

---
+++ ClockNumberProblem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClockNumberProblem
+//
+//  Created by Paul Bryan on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
